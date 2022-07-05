0fab6d21a3668033b3537466c3cbb647ad221f11
myismilk <85498810+myismilk@users.noreply.github.com>
2022-07-05 15:12:53 +0800

    Update UploadTest.java

--- service/service_oss/src/main/java/com/wangkaiping/oss/test/ossTest/UploadTest.java
+++ service/service_oss/src/main/java/com/wangkaiping/oss/test/ossTest/UploadTest.java
@@ -10,14 +10,14 @@ import java.io.*;
 public class UploadTest {
     public static void main(String[] args) {
         // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
-        String endpoint = "oss-cn-shenzhen.aliyuncs.com";
+        String endpoint = "";
 // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
         String accessKeyId = "";
         String accessKeySecret = "";
 // 填写Bucket名称，例如examplebucket。
-        String bucketName = "laowangedu";
+        String bucketName = "";
 // 填写文件名。文件名包含路径，不包含Bucket名称。例如exampledir/exampleobject.txt。
-        String objectName = "2022/mm.jpg";
+        String objectName = "";
 
         OSS ossClient = null;
         try {
